f60aa117bc9fb748be6635b5e741f264f1746c15
Nazar Halokha <n.galokha@gamil.com>
2017-11-22 21:45:23 +0200

    change setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DIBS-Agent',
+    version='0.0.1dev',
+    packages=['dibs-agent',],
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read(),
+)
